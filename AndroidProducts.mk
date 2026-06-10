64fd3f0813b9ecdf1077b99b9a2ac161706bbc8d
CoreShiftD <278102187+CoreShiftD@users.noreply.github.com>
2026-06-10 16:29:06 +0000

    Add matrix-based GitHub Actions workflow to build TWRP, PBRP, SHRP, and OrangeFox recoveries in parallel with 24GB swap and Slimhub cleanup
    Signed-off-by: CoreShiftD <278102187+CoreShiftD@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_INOI_A75.mk
+    $(LOCAL_DIR)/twrp_INOI_A75.mk \
+    $(LOCAL_DIR)/pb_INOI_A75.mk \
+    $(LOCAL_DIR)/shrp_INOI_A75.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_INOI_A75-eng
+    twrp_INOI_A75-eng \
+    pb_INOI_A75-eng \
+    shrp_INOI_A75-eng
